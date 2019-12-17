37bf8b891265ccf3207372db54648a6a3995c794
Bell Eapen <github_public@gulfdoctor.net>
2019-12-17 08:31:21 -0500

    first commit

---
+++ src/lesion-mapper.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    name: 'LesionMapper', // vue component name
+    data() {
+        return {
+            counter: 5,
+            initCounter: 5,
+        };
+    },
+}
+</script>
+
+<template>
+    <div class="lesion-mapper">
+        <p>The counter is set to <b>{{counter}}</b>.</p>
+        <button @click="counter += 1">Click +1</button>
+        <button @click="counter -= 1">Click -1</button>
+        <button @click="counter = initCounter">Reset</button>
+    </div>
+</template>
+
+<style scoped>
+    .lesion-mapper {
+        display: block;
+        width: 400px;
+        margin: 25px auto;
+        border: 1px solid #ccc;
+        background: #eaeaea;
+        text-align: center;
+        padding: 25px;
+    }
+    .lesion-mapper p {
+        margin: 0 0 1em;
+    }
+</style>
