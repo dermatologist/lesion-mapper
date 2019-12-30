6536f596bc2066138f97e490aa5ee92a41e8c8a9
beapen <github@gulfdoctor.net>
2019-12-29 21:24:34 -0500

    test: test in progress. partly working

--- test/lesion-mapper.spec.js
+++ test/lesion-mapper.spec.js
@@ -1,10 +1,23 @@
-import { shallowMount } from '@vue/test-utils'
-import Counter from '../src/lesion-mapper.vue'
+import { Mount } from '@vue/test-utils'
+import LesionMapper from '../src/lesion-mapper.vue'
 
-describe('Counter.vue', () => {
-  it('increments count when button is clicked', () => {
-    const wrapper = shallowMount(Counter)
-    wrapper.find('button').trigger('click')
-    expect(wrapper.find('div').text()).toMatch('1')
+/*
+This throws a TypeError: Object(...) is not a function error
+I am not sure why this happens
+This may be related to the canvas error
+
+Refer: https://vue-test-utils.vuejs.org/guides/testing-single-file-components-with-mocha-webpack.html
+See also: webpack.config.js
+*/
+describe('LesionMapper', () => {
+  it('mount the lesionmapper component', () => {
+      //shallowMount to Mount
+    try{  
+        const wrapper = Mount(LesionMapper)
+        expect(wrapper.find('debug').text()).toMatch('')
+    } catch (error){
+        console.log(error)
+    }
+    // wrapper.find('button').trigger('click')
   })
 })
